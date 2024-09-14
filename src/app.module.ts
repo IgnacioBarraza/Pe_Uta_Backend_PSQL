4b0168ea256e6cfe865db0de5d44b098b8669104
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-09-14 13:43:18 -0300

    🔐 Using .env file to connect postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/user.module';
 import { ProjectsModule } from './modules/project.module';
 import { SubjectModule } from './modules/subject.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'peuta_nest',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('HOST'),
+        port: +configService.get<number>('PORT'),
+        username: configService.get('USER'),
+        password: configService.get('PASSWORD'),
+        database: configService.get('DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     UsersModule,
     ProjectsModule,
