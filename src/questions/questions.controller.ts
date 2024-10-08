709ad5b7e0f61ee5926f97174b2a89b085b4d032
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-10-08 20:50:52 -0300

    ✨ Questions controller and service

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Questions } from 'src/entities/questions.entity';
+import { CreateQuestionDto } from 'src/utils/interfaces';
+import { QuestionsService } from './questions.service';
 
 @Controller('questions')
-export class QuestionsController {}
+export class QuestionsController {
+  constructor(private readonly questionsService: QuestionsService) {}
+
+  @Get()
+  async findAll(): Promise<Questions[]> {
+    return this.questionsService.findAll();
+  }
+
+  @Post()
+  async create(
+    @Body() createQuestionsDto: CreateQuestionDto,
+  ): Promise<Questions> {
+    return this.questionsService.createQuestion(createQuestionsDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.questionsService.deleteQuestion(id);
+  }
+}
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Questions } from '../entities/questions.entity';
+import { CreateQuestionDto } from '../utils/interfaces';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class QuestionsService {}
+export class QuestionsService {
+  constructor(
+    @InjectRepository(Questions)
+    private readonly questionRepository: Repository<Questions>,
+  ) {}
+
+  async findAll(): Promise<Questions[]> {
+    return this.questionRepository.find();
+  }
+
+  async createQuestion(
+    createQuestionDto: CreateQuestionDto,
+  ): Promise<Questions> {
+    const question = this.questionRepository.create(createQuestionDto);
+    return this.questionRepository.save(question);
+  }
+
+  async deleteQuestion(id: string): Promise<void> {
+    await this.questionRepository.delete(id);
+  }
+}
