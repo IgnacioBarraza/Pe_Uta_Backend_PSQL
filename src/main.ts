cf9cb107fa8047d1288c30621d61cd28b62a8b11
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-10-12 23:13:16 -0300

    🐛 Enable all origins CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL, // Replace with your frontend URL
+    origin: '*', // Replace with your frontend URL
     credentials: true, // If you need to send cookies
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed methods
     allowedHeaders: 'Content-Type,Authorization', // Allowed headers
